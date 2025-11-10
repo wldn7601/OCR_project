2c0bd06a7d9cc12a76adfeafed9036b178ae57b7
wldn7601 <wldn76011@gmail.com>
2025-11-10 00:50:54 +0000

    실제 실행 파일

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+from models.models import db, User
+
+def create_app():
+    app = Flask(__name__, template_folder="templates", static_folder="static")
+    load_dotenv()
+    CORS(app)
+
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev_secret")
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ocr_project.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+
+    from routes.auth_routes import auth_bp
+    from routes.admin_routes import admin_bp
+    from routes.user_routes import user_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(admin_bp)
+    app.register_blueprint(user_bp)
+
+    with app.app_context():
+        db.create_all()
+        if not User.query.filter_by(user_id="admin").first():
+            admin = User(user_id="admin", email="admin@example.com")
+            admin.set_password("admin")
+            db.session.add(admin)
+            db.session.commit()
+            print("[INFO] 기본 관리자 계정(admin/admin) 생성 완료")
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host="0.0.0.0", port=5000, debug=True)

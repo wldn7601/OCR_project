85c6a7e9a031ce61b3d0c2356994f465f00a5e11
wldn7601 <wldn76011@gmail.com>
2025-11-10 01:01:40 +0000

    DB 테이블 모델

---
+++ models/models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    user_id = db.Column(db.String(30), primary_key=True)
+    email = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)

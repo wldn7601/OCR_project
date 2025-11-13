9d2e57d2e0d282a69b68a0bd3a324e9455251f82
wldn7601 <wldn76011@gmail.com>
2025-11-13 07:24:27 +0000

    실제 실행 파일

--- main.py
+++ main.py
@@ -35,9 +35,38 @@ with app.app_context():
 
 
 if __name__ == "__main__":
+
     # gunicorn --bind 0.0.0.0:8000 main:app --daemo
     # :포트번호 .py파일의 이름:flask 객체이름 --deamon
+
+    # 가상 환경에서 pip install gevent
+    # gunicorn에서 멀티 쓰레드 : 기본적으로 sync worker
+    # gevent : async로 변경
+    # connections : worker가 최대 동시 접속
+    # gunicorn --worker-class gevent --worker 3 --worker-connections 1000 --bind 0.0.0.0:8000 main:app
+
     
+    # systemd 서비스 등록
+    # Gunicorn을 시스템 서비스로 실행하려면 다음 파일을 생성합니다.
+    # $ sudo nano /etc/systemd/system/[원하는 이름].service
+
+    # [[원하는 이름].service]
+    # [Unit]
+    # Description=Gunicorn Flask Application
+    # After=network.target
+
+    # [Service]
+    # User=ubuntu
+    # Group=ubuntu
+    # WorkingDirectory=/home/ubuntu/OCR_Project/myapp
+    # Environment="PATH=/home/ubuntu/OCR_Project/venv312/bin"
+    # ExecStart=/home/ubuntu/OCR_Project/venv312/bin/gunicorn --worker-class gevent --workers 9 --bind 0.0.0.0:8000 --worker-connections 1000 main:app
+
+    # [Install]
+    # WantedBy=multi-user.target
+
+
     # ps aux | grep gunicorn
     # 실행 중인 gunicorn 보기
+
     app.run(host="0.0.0.0", port=5000, debug=True)
